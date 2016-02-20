c13d9157b0358008760f4c1f3a1da91cede81fda
q6770570 <354800312@qq.com>
2016-02-21 03:01:44 +0800

    first push

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  resources :products
+  get 'home/index'
+
+  get 'home/welcome'
+
+  get 'home/about'
+
+  get 'home/contact'
+
+  
+
+  resources :products do
+    resources :variants
+  end
+  resources :user, only: [:index, :show]
+  resources :products, except: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
